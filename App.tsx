f3ac529b1ff3c88ccd8a4c26562b2d42faacf38f
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-23 06:34:50 -0400

    the header was made

--- App.tsx
+++ App.tsx
@@ -1,8 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './src/screens/Home';
+import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+import AppLoading from 'expo-app-loading';
+import theme from './src/styles/theme';
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import Logo from '../../assets/logo.svg';
+import { Container, Header, HeaderContentent, TotalCars } from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from './styles';
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Header>
+        <HeaderContentent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContentent>
+      </Header>
     </Container>
   );
 }
