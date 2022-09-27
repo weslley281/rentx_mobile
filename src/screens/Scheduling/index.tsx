688cca7543cffc1ce3b01b73fcb424c5196cd6da
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-27 06:33:33 -0400

    Calendar was created

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -16,6 +16,7 @@ import {
 } from './styles';
 import { StatusBar } from 'react-native';
 import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 
 export function Scheduling() {
   const theme = useTheme();
@@ -49,7 +50,9 @@ export function Scheduling() {
         </RentalPeriod>
       </Header>
 
-      <Content></Content>
+      <Content>
+        <Calendar />
+      </Content>
 
       <Footer>
         <Button title="Confirmar" />
