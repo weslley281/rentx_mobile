1cd933172ccea7d8d44b5b12194200de3a66d471
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-09-28 12:30:58 -0400

    typing parameters in navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      Scheduling: undefined;
+      SchedulingDetails: undefined;
+      SchedulingConfirm: undefined;
+      CarDeatails: undefined;
+    }
+  }
+}
