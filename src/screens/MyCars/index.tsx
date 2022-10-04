e00a0dad9d949c47adc566d836fa4a7c9c6f6df3
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-03 23:35:35 -0400

    a

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import { CarDTO } from '../../dtos/CarDTO';
+import { api } from '../../services/api';
+import { NavigationProps } from '../../dtos/NavigationDTO';
+import { Container, Header, SubTitle, Title } from './styles';
+import { useNavigation } from '@react-navigation/core';
 
-import { Container } from './styles';
+interface Params extends NavigationProps {
+  car: CarDTO;
+  goBack: () => void;
+}
 
 export function MyCars() {
-  return <Container></Container>;
+  const theme = useTheme();
+  const navigation = useNavigation<Params>();
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get('/schedules_byuser?user_id=1');
+
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  });
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton color={theme.colors.shape} onPress={handleBack} />
+
+        <Title>
+          Excolha uma{'\n'}data para início e{'\n'}termino do Aluguel
+        </Title>
+
+        <SubTitle>Conforto e segurança</SubTitle>
+      </Header>
+    </Container>
+  );
 }
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,5 +1,34 @@
-import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled, { css } from 'styled-components/native';
+
+interface DateValueProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 325px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: center;
+  padding: 25px;
+  padding-top: ${getStatusBarHeight() + 30}px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(34)}px;
+  margin-top: 24px;
+`;
+
+export const SubTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(34)}px;
+  margin-top: 24px;
 `;
