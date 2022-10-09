e49d133e992369ab18fd1c721f4fea6a5601e7bb
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-09 11:20:38 -0400

    a

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,24 +1,48 @@
-import { Feather } from '@expo/vector-icons';
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInputProps } from 'react-native';
 import { useTheme } from 'styled-components';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
 
 import { Container, IconContainer, InputText } from './styles';
 
 interface InputProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>['name'];
+  value?: string;
 }
 
-export function Input({ iconName, ...rest }: InputProps) {
-  const theme = useTheme();
+export const Input: React.FC<InputProps> = ({ iconName, value, ...rest }) => {
+  const { colors } = useTheme();
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  }
 
   return (
     <Container>
-      <IconContainer>
-        <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+      <IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          size={RFValue(22)}
+          color={isFocused || isFilled ? colors.main : colors.text_detail}
+        />
       </IconContainer>
 
-      <InputText keyboardType="email-address" autoCapitalize="none" {...rest} />
+      <InputText
+        isFocused={isFocused}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        value={value}
+        {...rest}
+      />
     </Container>
   );
-}
+};
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,25 +1,43 @@
+import { TextInput } from 'react-native';
+import styled, { css } from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
-import styled from 'styled-components/native';
+
+interface Props {
+  isFocused: boolean;
+}
 
 export const Container = styled.View`
+  width: 100%;
   flex-direction: row;
   margin-bottom: 8px;
 `;
 
-export const IconContainer = styled.View`
-  height: 56px;
-  width: 55px;
-  justify-content: center;
+export const IconContainer = styled.View<Props>`
+  width: ${RFValue(55)}px;
+  height: ${RFValue(55)}px;
   align-items: center;
-  margin-right: 2px;
-  background-color: ${({ theme }) => theme.colors['background_secondary']};
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+  margin-right: ${RFValue(2)}px;
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
 
-export const InputText = styled.TextInput`
+export const InputText = styled(TextInput)<Props>`
   flex: 1;
-  background-color: ${({ theme }) => theme.colors['background_secondary']};
-  color: ${({ theme }) => theme.colors['text']};
-  font-family: ${({ theme }) => theme.fonts['primary_400']};
-  font-size: ${RFValue(15)}px;
-  padding: 0 23px;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(14)}px;
+  padding: 0 ${RFValue(22)}px;
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
